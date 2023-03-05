c60bac3504c52826212e668752224be4d9ecadda
Hypie-u <116455922+Hypie-u@users.noreply.github.com>
2023-03-05 18:47:08 +0300

    pro_1
    reloaded projects

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name  generator")
+city = input("which city did you grow up in?")
+pet = input("What is the name of pet")
+print("Your band name could beceyha n  " + city + " " + pet)
